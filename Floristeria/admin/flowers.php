5109f46058fda085d3920dfb5572a90512df2204
AlejandroBS <alejandrobiergeserrano@gmail.com>
2014-10-15 14:07:15 +0200

    flores

--- Floristeria/admin/flowers.php
+++ Floristeria/admin/flowers.php
@@ -9,22 +9,32 @@ $category = array(0 => "Ramos", 1 => "Centros", 2 => "Bodas", 3 => "Plantas", 4 
     require_once '../libs/Flower.php';
     require_once './Model/FlowerModel.php';
     ?>
+    
     <link type="text/css" rel="stylesheet" href="css/tiny.css" media="all">
     <script type="text/javascript" src="js/tinyeditor.js"></script>
+    <script src="../js/jquery-1.7.1.min.js" ></script>   
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#btn_insertar").click(function(){
+                console.log("ahora");
+            });
+        });
+        
+    </script>
     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
         <h1 class="page-header">Añadir nueva flor</h1>
-        <form id="for" role="form">
+        <form id="for" role="form" >
             <div class="form-group">
                 <label for="email">Nombre de la Flor:</label>
-                <input type="email" class="form-control" id="email">
+                <input type="nombre-flor" class="form-control" id="email" name="nombre">
             </div>
             <div class="form-group">
                 <label for="pwd">Precio:</label>
-                <input type="text" class="form-control" id="pwd">
+                <input type="text" class="form-control" id="pwd" name="precio">
             </div>
             <div class="form-group">
                 <label for="pwd">Sección:</label>
-                <select class="form-control">
+                <select class="form-control" name="categoria">
                     <?php foreach ($category as $key => $value): ?>
                         <option value="<?php echo($key); ?>"><?php echo($value); ?></option>
                     <?php endforeach; ?>
@@ -32,38 +42,42 @@ $category = array(0 => "Ramos", 1 => "Centros", 2 => "Bodas", 3 => "Plantas", 4 
             </div>
             <div class="form-group">
                 <label for="ejemplo_archivo_1">Adjuntar una imagen:</label>
-                <input type="file" id="ejemplo_archivo_1">
+                <input type="file" id="ejemplo_archivo_1" name="file">
                 <p class="help-block">Ejemplo de texto de ayuda.</p>
             </div>
             <div class="form-group">
                 <label>Descripción:</label>
-                <textarea id="input" style="width:400px; height:200px"></textarea>
+                <textarea id="editor_texto" style="width:400px; height:200px"></textarea>
             </div>
-            <button type="submit" class="btn btn-default">Guardar</button>
+            <button id="btn_insertar" class="btn btn-default" >Guardar</button>
+            <img name="img_load" id="img-load" src=""/>
         </form>
+
         <script type="text/javascript">
-            new TINY.editor.edit('editor', {
-                id: 'input',
-                width: 800,
-                height: 200,
-                cssclass: 'te',
-                controlclass: 'tecontrol',
-                rowclass: 'teheader',
-                dividerclass: 'tedivider',
-                controls: ['bold', 'italic', 'underline', 'strikethrough', '|', 'subscript', 'superscript', '|',
-                    'orderedlist', 'unorderedlist', '|', 'outdent', 'indent', '|', 'leftalign',
-                    'centeralign', 'rightalign', 'blockjustify', '|', 'unformat', '|', 'undo', 'redo', 'n',
-                    'font', 'size', 'style', '|', 'image', 'hr', 'link', 'unlink', '|', 'cut', 'copy', 'paste', 'print'],
-                footer: false,
-                fonts: ['Verdana', 'Arial', 'Georgia', 'Trebuchet MS'],
-                xhtml: true,
-                cssfile: 'style.css',
-                bodyid: 'editor',
-                footerclass: 'tefooter',
-                toggle: {text: 'show source', activetext: 'show wysiwyg', cssclass: 'toggle'},
-                resize: {cssclass: 'resize'}
-            });
+        var editor = new TINY.editor.edit('editor', {
+            id: 'editor_texto',
+            width: 800,
+            height: 200,
+            cssclass: 'te',
+            controlclass: 'tecontrol',
+            rowclass: 'teheader',
+            dividerclass: 'tedivider',
+            controls: ['bold', 'italic', 'underline', 'strikethrough', '|', 'subscript', 'superscript', '|',
+                'orderedlist', 'unorderedlist', '|', 'outdent', 'indent', '|', 'leftalign',
+                'centeralign', 'rightalign', 'blockjustify', '|', 'unformat', '|', 'undo', 'redo', 'n',
+                'font', 'size', 'style', '|', 'image', 'hr', 'link', 'unlink', '|', 'cut', 'copy', 'paste', 'print'],
+            footer: false,
+            fonts: ['Verdana', 'Arial', 'Georgia', 'Trebuchet MS'],
+            xhtml: true,
+            cssfile: 'style.css',
+            bodyid: 'editor',
+            footerclass: 'tefooter',
+            toggle: {text: 'show source', activetext: 'show wysiwyg', cssclass: 'toggle'},
+            resize: {cssclass: 'resize'}
+        });
+
         </script>
+
     </div>
 </div>
 </div>
---
+++ Floristeria/admin/posts/insertarflor.php
@@ -0,0 +1,9 @@
+<?php
+
+/*$nombre = $_REQUEST['nombre'];
+$precio = $_REQUEST['precio'];
+$categoria = $_REQUEST['categoria'];
+$file = $_REQUEST['file'];*/
+$descripcion = $_GET['editor_texto'];
+
+echo "jo";
