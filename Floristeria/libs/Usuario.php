5a4dedc76fd3a03f9423138af85b90faa8e9f61a
LuisEsc <acidobox@gmail.com>
2014-10-15 09:48:19 +0200

    Se

--- Floristeria/libs/Usuario.php
+++ Floristeria/libs/Usuario.php
@@ -13,39 +13,39 @@ class Usuario {
     public $codpostal;
     public $provincia;
     public $pais;
-    
-    public static function arrayToUser($array){
-        $this->email     = $array['email'];
-        $this->password  = $array['password'];
-        $this->nombre    = $array['nombre'];
-        $this->dni       = $array['dni'];
+
+    public static function arrayToUser($array) {
+        $this->email = $array['email'];
+        $this->password = $array['password'];
+        $this->nombre = $array['nombre'];
+        $this->dni = $array['dni'];
         $this->apellidos = $array['apellidos'];
-        $this->telefono  = $array['telefono'];
+        $this->telefono = $array['telefono'];
         $this->direccion = $array['direccion'];
         $this->localidad = $array['localidad'];
         $this->codpostal = $array['codpostal'];
         $this->provincia = $array['provincia'];
-        $this->pais      = $array['pais'];
+        $this->pais = $array['pais'];
         return $this;
     }
-    
+
     public function __construct($user) {
-        $this->email     = $user['email'];
-        $this->password  = $user['password'];
-        $this->nombre    = $user['nombre'];
-        $this->dni       = $user['dni'];
+        $this->email = $user['email'];
+        $this->password = $user['password'];
+        $this->nombre = $user['nombre'];
+        $this->dni = $user['dni'];
         $this->apellidos = $user['apellidos'];
-        $this->telefono  = $user['telefono'];
+        $this->telefono = $user['telefono'];
         $this->direccion = $user['direccion'];
         $this->localidad = $user['localidad'];
         $this->codpostal = $user['codpostal'];
         $this->provincia = $user['provincia'];
-        $this->pais      = $user['pais'];
+        $this->pais = $user['pais'];
     }
 
     public function actualizarDatos($email, $password, $nombre = "", $apellidos = "", $dni = "", $telefono = "", $direccion = "", $localidad = "", $codpostal = "", $provincia = "", $pais = "") {
-        $this->email = email;
-        $this->password = password;
+        $this->email = $email;
+        $this->password = $password;
         $this->nombre = $nombre;
         $this->apellidos = $apellidos;
         $this->dni = $dni;
