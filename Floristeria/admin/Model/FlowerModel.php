8e880a76eb0367ec85d45fb2c468de7274aa77d9
LuisEsc <acidobox@gmail.com>
2014-10-15 11:59:17 +0200

    actualizar

--- Floristeria/admin/Model/FlowerModel.php
+++ Floristeria/admin/Model/FlowerModel.php
@@ -1,38 +1,29 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of FlowerModel
  *
- * @author Esmonet
+ * @author Esmonet Soluciones Informáticas
  */
+require_once '../core/Connection.php';
+
 class FlowerModel {
 
-    private $flower = null;
-
-    public function __construct(Flower $flower) {
-        $this->flower = $flower;
-    }
-
     // Guarda la flor en la base de datos
-    public function save() {
+    public static function save(Flower $flower) {
+
         $sql = "INSERT INTO table_name (id, title, description)";
         $sql .= "VALUES (null , {$this->flower->title}}, {$this->flower})";
     }
 
     // Actualiza la flor en la base de datos
-    public function update() {
+    public static function update(Flower $flower) {
         $sql = "UPDATE flowers SET ContactName='Alfred Schmidt', City='Hamburg' WHERE CustomerName='Alfreds Futterkiste';";
     }
 
     // Elimina la flor de la base de datos
-    public function delete() {
+    public function delete(Flower $flower) {
         $sql = "DELETE FROM flowers WHERE id=1;";
     }
-    
+
 }
--- Floristeria/admin/flowers.php
+++ Floristeria/admin/flowers.php
@@ -1,1 +1,74 @@
 <?php
+include_once './inc/Session.php';
+$category = array(0 => "Ramos", 1 => "Centros", 2 => "Bodas", 3 => "Plantas", 4 => "Funerarios");
+?>
+<html lang="es">
+    <?php
+    require_once './inc/header_struct.php';
+    require_once '../core/Connection.php';
+    require_once '../libs/Flower.php';
+    require_once './Model/FlowerModel.php';
+    ?>
+    <link type="text/css" rel="stylesheet" href="css/tiny.css" media="all">
+    <script type="text/javascript" src="js/tinyeditor.js"></script>
+    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <h1 class="page-header">Añadir nueva flor</h1>
+        <form id="for" role="form">
+            <div class="form-group">
+                <label for="email">Nombre de la Flor:</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Precio:</label>
+                <input type="text" class="form-control" id="pwd">
+            </div>
+            <div class="form-group">
+                <label for="pwd">Sección:</label>
+                <select class="form-control">
+                    <?php foreach ($category as $key => $value): ?>
+                        <option value="<?php echo($key); ?>"><?php echo($value); ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="ejemplo_archivo_1">Adjuntar una imagen:</label>
+                <input type="file" id="ejemplo_archivo_1">
+                <p class="help-block">Ejemplo de texto de ayuda.</p>
+            </div>
+            <div class="form-group">
+                <label>Descripción:</label>
+                <textarea id="input" style="width:400px; height:200px"></textarea>
+            </div>
+            <button type="submit" class="btn btn-default">Guardar</button>
+        </form>
+        <script type="text/javascript">
+            new TINY.editor.edit('editor', {
+                id: 'input',
+                width: 800,
+                height: 200,
+                cssclass: 'te',
+                controlclass: 'tecontrol',
+                rowclass: 'teheader',
+                dividerclass: 'tedivider',
+                controls: ['bold', 'italic', 'underline', 'strikethrough', '|', 'subscript', 'superscript', '|',
+                    'orderedlist', 'unorderedlist', '|', 'outdent', 'indent', '|', 'leftalign',
+                    'centeralign', 'rightalign', 'blockjustify', '|', 'unformat', '|', 'undo', 'redo', 'n',
+                    'font', 'size', 'style', '|', 'image', 'hr', 'link', 'unlink', '|', 'cut', 'copy', 'paste', 'print'],
+                footer: false,
+                fonts: ['Verdana', 'Arial', 'Georgia', 'Trebuchet MS'],
+                xhtml: true,
+                cssfile: 'style.css',
+                bodyid: 'editor',
+                footerclass: 'tefooter',
+                toggle: {text: 'show source', activetext: 'show wysiwyg', cssclass: 'toggle'},
+                resize: {cssclass: 'resize'}
+            });
+        </script>
+    </div>
+</div>
+</div>
+
+
+
+
+</html>
