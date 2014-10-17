f8868f293dce53720b0ac29cc03f20e7d8d0bd70
LuisEsc <acidobox@gmail.com>
2014-10-17 10:22:03 +0200

    Imagenes

--- Floristeria/resources/image.php
+++ Floristeria/resources/image.php
@@ -1,15 +1,22 @@
 <?php
 
 $id = filter_input(INPUT_GET, 'ID', FILTER_VALIDATE_INT);
+
+
 // Compruebo que el valor devuelto de la función sea diferente a false y a null 
 if (($id != FALSE) && ($id != NULL)) {
-    // Añado las librerias
+    // Añado las librerías
     require_once '../core/Connection.php';
     require_once '../libs/Flower.php';
     require_once '../model/FlowersModel.php';
 
-    $flower = FlowersModel::getFlowerById($id);
+
+    if (($flower = FlowersModel::getFlowerById($id)) != null) {
+        header("Content-Type: image/jpeg");
+    }
     print_r($flower);
+    header("Content-Type: image/jpeg");
+    header("Content-Length: " . strlen($cnt));
 }
 
 abstract class ImageType {
