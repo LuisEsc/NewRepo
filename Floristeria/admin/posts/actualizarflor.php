f04a693eeab6d6eda2edd819b953dd7ab3294d4a
AlejandroBS <alejandrobiergeserrano@gmail.com>
2014-10-27 12:47:52 +0100

    cambios

--- Floristeria/admin/posts/actualizarflor.php
+++ Floristeria/admin/posts/actualizarflor.php
@@ -5,6 +5,7 @@ require_once '../../core/Connection.php';
 require_once '../../model/FlowersModel.php';
 require_once '../../libs/Flower.php';
 require_once '../libs/BinaryImage.php';
+//require_once 'Imagick.php';
 
 $id = $_REQUEST['id'];
 $nombre = $_REQUEST['nombre'];
